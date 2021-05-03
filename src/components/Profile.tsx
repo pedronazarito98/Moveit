e3434aad7603ac62025e695ff98bac122aa9a39a
pedronazarito98 <57152268+pedronazarito98@users.noreply.github.com>
2021-05-03 13:44:10 -0300

    Primeira Versão (não atualizada)

---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/pedronazarito98.png" alt="PedroNaza" />
+
+      <div>
+        <strong>Pedro Nazarito</strong>
+        <p>
+          <img src="icons/level.svg" alt="level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
